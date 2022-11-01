3a7c79daf67460e6d5074d024347fc3ba9a3574a
Demin Santana <39230176+deminsantana@users.noreply.github.com>
2022-11-01 17:09:14 -0400

    Initial commit

---
+++ frontend/src/layouts/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div class="cintilloCyber">
+        <div class="wrapcintillo">
+          <span>
+            ¡Aprovecha nuestros <strong>Black Days</strong> y mensaje.
+          </span>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Header;
---
+++ frontend/src/layouts/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+
+const Navbar = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
